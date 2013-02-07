4124f46cad6b4c5dd2b0fea5a35dc1c7c39cde0b
yeray <yeray@10.37.129.2>
2013-02-07 11:15:13 +0000

    SintacticTree v. 2

--- src/sintactictree/Node.java
+++ src/sintactictree/Node.java
@@ -3,19 +3,34 @@ public abstract class Node {
     private Object info;
     private Node right, left;
     
-    public void setInfo(Object info){
+    public Node (Object info){
         this.info = info;
-    }
-    
-    public Object getInfo(){
-        return info;
-    }
-    
-    public Node (){
-        this.info = null;
         this.right = null;
         this.left = null;
     }
     
+    public Object getInfo() {
+        return info;
+    }
+
+    public Node getNodeRight() {
+        return right;
+    }
+      
+    public Node getNodeLeft() {
+        return left;
+    }
+
+    public void setInfo(Object info){
+        this.info = info;
+    }
+    public void setNodeRight(Node rnode) {
+        this.right = rnode;
+    }
+
+    public void setNodeLeft(Node lnode) {
+        this.left = lnode;
+    }
+
     public abstract boolean Evaluate();
 }
