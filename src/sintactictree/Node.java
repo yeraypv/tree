ed74b761368e100571cc04f135e92e202a6317e5
yeray <yeray@10.37.129.2>
2013-02-18 15:45:39 +0000

    Modificated Node.java

--- src/sintactictree/Node.java
+++ src/sintactictree/Node.java
@@ -1,7 +1,26 @@
 package sintactictree;
 
 public abstract class Node {
-    public Object info;
-    public Node right, left;
+    private Object info;
+    private Node right, left;
+    public Object getNodeInfo(){
+        return info;
+    }
+    
+    public Node getNodeRight(){
+        return right;
+    }
+    
+    public Node getNodeLeft(){
+        return left;
+    }
+    
+    public void setNodeRight(Node node){
+        right = node;
+    }
+    
+    public void setNodeLeft(Node node){
+        left = node;
+    }
     abstract Object Evaluate();
 }
