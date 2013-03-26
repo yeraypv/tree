fe3ed6b13e59d0350665a51abf44bc64a08263e6
yeray <yeray@10.37.129.2>
2013-03-26 20:59:13 +0000

    Refactoring with reflection experimental 1

--- src/evaluator/calculators/CalculateEvaluate.java
+++ src/evaluator/calculators/CalculateEvaluate.java
@@ -9,52 +9,10 @@ import java.util.HashMap;
 
 public class CalculateEvaluate implements Evaluate {
 
-    //TODO: tiene que estar en otra clase operador
-    private static HashMap<String, Operator> operatorMap;
-
-    static {
-        operatorMap = new HashMap<>();
-        operatorMap.put("+", Operator.add);
-        operatorMap.put("-", Operator.substract);
-        operatorMap.put("*", Operator.mul);
-        operatorMap.put("/", Operator.div);
-    }
-
-    public static Operator getOperator(String value) {
-        return operatorMap.get(value);
-    }
-
+    //TODO: Refactoring reflection
     @Override
-    public Type calculate(Operator operator, Type arg0, Type arg1) {         
-        Calculator calculator = selectCalculators(arg0, arg1);       
-        if (operator == null || calculator == null) {
-            return null;
-        }
-        try {
-            Method method = calculator.getClass().getMethod(operator.getName(), arg0.getValue().getClass(), arg1.getValue().getClass());
-            return convertType(method.invoke(calculator, arg0.getValue(), arg1.getValue()));
-        } catch (NoSuchMethodException | SecurityException ex) {
-            return null;
-        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
-            return null;
-        }
- 
-    }
-    
-    private Type convertType(Object object) {
-        if (object instanceof Integer) {
-            return new evaluator.types.Integer((Integer) object);
-        }
-        if (object instanceof Double) {
-            return new evaluator.types.Double((Double) object);
-        }
-        if (object instanceof String) {
-            return new evaluator.types.String((String) object);
-        }
-        return null;
+    public Type calculate(Operator operator, Type arg0, Type arg1) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    private Calculator selectCalculators(Type arg0, Type arg1) {  
-        return(arg0 == null || arg1 == null)?null:new CoreNumberCalculator();
-    }
 }
