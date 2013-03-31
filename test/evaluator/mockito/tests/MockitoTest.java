2f2d976a7e83717836f14f988676c72b8fad5c71
yeray <yeray@10.37.129.2>
2013-03-31 13:12:15 +0100

    Tests

--- test/evaluator/mockito/tests/MockitoTest.java
+++ test/evaluator/mockito/tests/MockitoTest.java
@@ -10,7 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
-public class BinaryOperatorsMockitoTest {
+public class MockitoTest {
 
     private Binary binaryOperation;
     
